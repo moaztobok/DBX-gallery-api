628675c868591050a0635bb2cd1208bb3a632af8
moaztobok <moaztobok@gmail.com>
2024-06-24 21:05:26 +0100

    dbx-gallery first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controller');
+
+/* GET home page. */
+router.get('/', controller.home);
+router.get('/login', controller.login);
+router.get('/oauthredirect',controller.oauthredirect);
+
+module.exports = router;
